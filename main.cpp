f602348b4ebb4d8ce9e5ece46f156d379de29f0c
AndK <karpukhin_av@mail.ru>
2023-06-01 01:39:23 +0300

    partly working settings menu

--- main.cpp
+++ main.cpp
@@ -5,6 +5,43 @@
 #include <vector>
 
 #include "roguelike/control.hpp"
+// #include "roguelike/menu.hpp"
+
+int draw_menu(std::vector<std::string> &menu_points)
+{
+    system("cls");
+    int pos = 0;
+    while (1) {
+        clearConsole();
+        SetConsoleCursorPosition(hStdOut, makeCoord(0, 0));
+        for (int i = 0; i < menu_points.size(); i++)
+        {
+            if (i == pos){
+                SetConsoleTextAttribute(hStdOut, color_cyan);
+                std::cout<<"-> ";
+            }else{
+                std::cout<<"   ";
+            }
+            std::cout<<menu_points[i]<<"\n";
+            SetConsoleTextAttribute(hStdOut, color_white);
+        }
+
+        char input;
+        input = _getwch();
+        if (input == 'w' && pos > 0) {
+            pos--;
+        } else if (input == 's' && pos < 2) {
+            pos++;
+        }else if (input == 13) {
+            break;
+        }
+        else{
+            std::cout<<input<<"\n";
+            while(1){}
+        }
+    }
+    return pos;
+}
 
 
 void start_game()
@@ -39,40 +76,27 @@ void start_game()
               << "Game over!" << std::endl;
 }
 
-int main() {
-    system("cls");
-
-    std::vector<std::string> menu_points{"Start game", "Settings", "Exit"};
-    int pos = 0;
-    while (1) {
-        clearConsole();
-        SetConsoleCursorPosition(hStdOut, makeCoord(0, 0));
-        for (int i = 0; i < menu_points.size(); i++)
-        {
-            if (i == pos){
-                SetConsoleTextAttribute(hStdOut, color_cyan);
-                std::cout<<"-> ";
-            }else{
-                std::cout<<"   ";
-            }
-            std::cout<<menu_points[i]<<"\n";
-            SetConsoleTextAttribute(hStdOut, color_white);
-        }
-
-        char input;
-        input = _getwch();
-        if (input == 'w' && pos > 0) {
-            pos--;
-        } else if (input == 's' && pos < 2) {
-            pos++;
-        }else if (input == 27) {
-            std::cout << std::endl
-                      << "Goodbye!" << std::endl;
-            break;
-        }else if (input == 13) {
-            break;
-        }
+void settings()
+{
+    std::vector<std::string> settings_points{"Change player simbol", "sound ON/OFF", "Back"};
+    int pos = draw_menu(settings_points);
+    switch (pos)
+    {
+    case 0:
+        //TODO
+        break;
+    case 1:
+        //TODO
+        break;
+    case 2:
+        break;
     }
+}
+
+void main_menu()
+{
+    std::vector<std::string> menu_points{"Start game", "Settings", "Exit"};
+    int pos = draw_menu(menu_points);
 
     switch (pos)
     {
@@ -80,10 +104,19 @@ int main() {
         start_game();
         break;
     case 1:
+        settings();
+        main_menu();
         break;
     case 2:
         std::cout<< "Goodbye!" << std::endl;
         break;
     }
+}
+
+int main() {
+    system("cls");
+
+    main_menu();
+
     return 0;
 }
