7d401c3dc2ae3b98cc6aa441f083914bfa040aff
vlad-vifel <vlad.vifel@yandex.ru>
2023-06-04 16:11:54 +0300

    doxygen comments for control entities print

--- roguelike/control.hpp
+++ roguelike/control.hpp
@@ -1,8 +1,15 @@
 #include "print.hpp"
 
+/// @brief Контроль игры
+/// @param map Карта подземелья
+/// @param map_clear Очищенная карта подземелья
+/// @param player Игрок
+/// @param input Ввод пользователя
+/// @return True, если игра пройдена, в противном случае False
 bool playGame(Map &map,
               Map &map_clear,
               Player &player,
               char input);
 
+/// @brief Победа в игре
 void win();
--- roguelike/print.hpp
+++ roguelike/print.hpp
@@ -3,10 +3,24 @@
 
 static HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
 
+/// @brief Отрисовка игры
+/// @param map Карта
+/// @param map_clear Очищенная карта
+/// @param player Игрок
 void drawGame(Map &map, Map &map_clear, Player &player);
 
+/// @brief Отрисовка статистики
+/// @param player Игрок
+/// @param entities Сущности
+/// @param roomNum Номер комнаты
+/// @param bonus Бонусное здоровье и бонусный урон
 void drawStatistics(Player &player, std::vector<Entity> &entities, int roomNum, std::vector<int> bonus);
 
+/// @brief Очистка консоли
 void clearConsole();
 
+/// @brief Создание координаты
+/// @param x Координата по оси Ox (вертикаль)
+/// @param y Координата по оси Oy (горизанталь)
+/// @return Координата
 COORD makeCoord(int x, int y);
